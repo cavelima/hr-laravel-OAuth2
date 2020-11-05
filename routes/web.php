9ac81a0459865554c951d77839dda10ec07a95ff
cavelima <vedran.anicic@molsoncoors.com>
2020-11-05 22:03:50 +0100

    Add README

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,16 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function() {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\EmployeeController@index')->middleware('auth');
 
 Auth::routes();
+
 
 Route::get('/auth/redirect/{provider}', 'App\Http\Controllers\Auth\LoginController@redirectToProvider');
 Route::get('/auth/{provider}/callback', 'App\Http\Controllers\Auth\LoginController@handleProviderCallback');
 
 Route::resource('employees', 'App\Http\Controllers\EmployeeController')->middleware('auth');
+Route::resource('employeesJSON', 'App\Http\Controllers\EmployeeJSONController')->middleware('auth');
 Route::resource('departments', 'App\Http\Controllers\DepartmentController')->middleware('auth');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
